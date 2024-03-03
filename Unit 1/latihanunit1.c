f0142a13a61eb7ed46b21596fa39b2b895fa74af
elsameilia <161608210+kucingbakso@users.noreply.github.com>
2024-03-04 00:02:47 +0700

    Add files via upload

---
+++ Unit 1/latihanunit1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int s1, v1, t1;
+int s2, v2, t2;
+
+int main () {
+    scanf("%d %d\n", &s1, &t1);
+    scanf("%d %d\n", &v1, &t2);
+    scanf("%d %d\n", &s2, &v2);
+
+    float hasilkec = (float) s1/t1;
+    float hasiljarak = (float) v1*t2;
+    float hasilwaktu = (float) s2/v2;
+    
+    printf("Kecepatannya adalah %.2f\n", hasilkec);
+    printf("Jaraknya adalah %.2f\n", hasiljarak);
+    printf("Waktunya adalah %.2f\n", hasilwaktu);
+
+    return 0;
+}
+
